c268994efb32fec45d30b7336429d885d4d6e37b
jdhanna <jdhanna7@gmail.com>
2022-12-20 21:40:08 -0500

    Heroku Deployment Changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.run(host="https://flea-flicker-projections.herokuapp.com/", port =5000)
+app.run(host="0.0.0.0", port =8080)
 
 from app import routes
